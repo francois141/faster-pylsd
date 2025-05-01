765b0765747e1e7d6fa177017b4682fe9a308737
François Costa <frankost.costa@gmail.com>
2025-05-01 20:46:28 +0200

    Add gaussian filter back
    (cherry picked from commit ca6bb6c9b0ffba8fae5a7ee404525cd9e769b5f0)

--- src/helpers.h
+++ src/helpers.h
@@ -1192,3 +1192,299 @@ static double rect_nfa(struct rect *rec, image_double angles, double logNT) {
 
   return nfa(pts, alg, rec->p, logNT); /* compute NFA value */
 }
+
+/*----------------------------------------------------------------------------*/
+/*----------------------- 'list of n-tuple' data type ------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/** 'list of n-tuple' data type
+
+    The i-th component of the j-th n-tuple of an n-tuple list 'ntl'
+    is accessed with:
+
+      ntl->values[ i + j * ntl->dim ]
+
+    The dimension of the n-tuple (n) is:
+
+      ntl->dim
+
+    The number of n-tuples in the list is:
+
+      ntl->size
+
+    The maximum number of n-tuples that can be stored in the
+    list with the allocated memory at a given time is given by:
+
+      ntl->max_size
+ */
+typedef struct ntuple_list_s {
+  unsigned int size;
+  unsigned int max_size;
+  unsigned int dim;
+  double *values;
+} *ntuple_list;
+
+/*----------------------------------------------------------------------------*/
+/** Free memory used in n-tuple 'in'.
+ */
+static void free_ntuple_list(ntuple_list in) {
+  if (in == nullptr || in->values == nullptr)
+    error("free_ntuple_list: invalid n-tuple input.");
+  free((void *) in->values);
+  free((void *) in);
+}
+
+/*----------------------------------------------------------------------------*/
+/** Create an n-tuple list and allocate memory for one element.
+    @param dim the dimension (n) of the n-tuple.
+ */
+static ntuple_list new_ntuple_list(unsigned int dim) {
+  ntuple_list n_tuple;
+
+  /* check parameters */
+  if (dim == 0) error("new_ntuple_list: 'dim' must be positive.");
+
+  /* get memory for list structure */
+  n_tuple = (ntuple_list) malloc(sizeof(struct ntuple_list_s));
+  if (n_tuple == nullptr) error("not enough memory.");
+
+  /* initialize list */
+  n_tuple->size = 0;
+  n_tuple->max_size = 1;
+  n_tuple->dim = dim;
+
+  /* get memory for tuples */
+  n_tuple->values = (double *) malloc(dim * n_tuple->max_size * sizeof(double));
+  if (n_tuple->values == nullptr) error("not enough memory.");
+
+  return n_tuple;
+}
+
+/*----------------------------------------------------------------------------*/
+/** Enlarge the allocated memory of an n-tuple list.
+ */
+static void enlarge_ntuple_list(ntuple_list n_tuple) {
+  /* check parameters */
+  if (n_tuple == nullptr || n_tuple->values == nullptr || n_tuple->max_size == 0)
+    error("enlarge_ntuple_list: invalid n-tuple.");
+
+  /* duplicate number of tuples */
+  n_tuple->max_size *= 2;
+
+  /* realloc memory */
+  n_tuple->values = (double *) realloc((void *) n_tuple->values,
+                                       n_tuple->dim * n_tuple->max_size * sizeof(double));
+  if (n_tuple->values == nullptr) error("not enough memory.");
+}
+
+/*----------------------------------------------------------------------------*/
+/** Add a 7-tuple to an n-tuple list.
+ */
+static void add_7tuple(ntuple_list out, double v1, double v2, double v3,
+                       double v4, double v5, double v6, double v7) {
+  /* check parameters */
+  if (out == nullptr) error("add_7tuple: invalid n-tuple input.");
+  if (out->dim != 7) error("add_7tuple: the n-tuple must be a 7-tuple.");
+
+  /* if needed, alloc more tuples to 'out' */
+  if (out->size == out->max_size) enlarge_ntuple_list(out);
+  if (out->values == nullptr) error("add_7tuple: invalid n-tuple input.");
+
+  /* add new 7-tuple */
+  out->values[out->size * out->dim + 0] = v1;
+  out->values[out->size * out->dim + 1] = v2;
+  out->values[out->size * out->dim + 2] = v3;
+  out->values[out->size * out->dim + 3] = v4;
+  out->values[out->size * out->dim + 4] = v5;
+  out->values[out->size * out->dim + 5] = v6;
+  out->values[out->size * out->dim + 6] = v7;
+
+  /* update number of tuples counter */
+  out->size++;
+}
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------- Gaussian filter ------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/** Compute a Gaussian kernel of length 'kernel->dim',
+    standard deviation 'sigma', and centered at value 'mean'.
+
+    For example, if mean=0.5, the Gaussian will be centered
+    in the middle point between values 'kernel->values[0]'
+    and 'kernel->values[1]'.
+ */
+static void gaussian_kernel(ntuple_list kernel, double sigma, double mean) {
+  double sum = 0.0;
+  double val;
+  unsigned int i;
+
+  /* check parameters */
+  if (kernel == nullptr || kernel->values == nullptr)
+    error("gaussian_kernel: invalid n-tuple 'kernel'.");
+  if (sigma <= 0.0) error("gaussian_kernel: 'sigma' must be positive.");
+
+  /* compute Gaussian kernel */
+  if (kernel->max_size < 1) enlarge_ntuple_list(kernel);
+  kernel->size = 1;
+  for (i = 0; i < kernel->dim; i++) {
+    val = ((double) i - mean) / sigma;
+    kernel->values[i] = exp(-0.5 * val * val);
+    sum += kernel->values[i];
+  }
+
+  /* normalization */
+  if (sum >= 0.0) for (i = 0; i < kernel->dim; i++) kernel->values[i] /= sum;
+}
+
+/*----------------------------------------------------------------------------*/
+/** Scale the input image 'in' by a factor 'scale' by Gaussian sub-sampling.
+
+    For example, scale=0.8 will give a result at 80% of the original size.
+
+    The image is convolved with a Gaussian kernel
+    @f[
+        G(x,y) = \frac{1}{2\pi\sigma^2} e^{-\frac{x^2+y^2}{2\sigma^2}}
+    @f]
+    before the sub-sampling to prevent aliasing.
+
+    The standard deviation sigma given by:
+    -  sigma = sigma_scale / scale,   if scale <  1.0
+    -  sigma = sigma_scale,           if scale >= 1.0
+
+    To be able to sub-sample at non-integer steps, some interpolation
+    is needed. In this implementation, the interpolation is done by
+    the Gaussian kernel, so both operations (filtering and sampling)
+    are done at the same time. The Gaussian kernel is computed
+    centered on the coordinates of the required sample. In this way,
+    when applied, it gives directly the result of convolving the image
+    with the kernel and interpolated to that particular position.
+
+    A fast algorithm is done using the separability of the Gaussian
+    kernel. Applying the 2D Gaussian kernel is equivalent to applying
+    first a horizontal 1D Gaussian kernel and then a vertical 1D
+    Gaussian kernel (or the other way round). The reason is that
+    @f[
+        G(x,y) = G(x) * G(y)
+    @f]
+    where
+    @f[
+        G(x) = \frac{1}{\sqrt{2\pi}\sigma} e^{-\frac{x^2}{2\sigma^2}}.
+    @f]
+    The algorithm first applies a combined Gaussian kernel and sampling
+    in the x axis, and then the combined Gaussian kernel and sampling
+    in the y axis.
+ */
+static image_double gaussian_sampler(image_double in, double scale,
+                                     double sigma_scale) {
+  image_double aux, out;
+  ntuple_list kernel;
+  unsigned int N, M, h, n, x, y, i;
+  int xc, yc, j, double_x_size, double_y_size;
+  double sigma, xx, yy, sum, prec;
+
+  /* check parameters */
+  if (in == nullptr || in->data == nullptr || in->xsize == 0 || in->ysize == 0)
+    error("gaussian_sampler: invalid image.");
+  if (scale <= 0.0) error("gaussian_sampler: 'scale' must be positive.");
+  if (sigma_scale <= 0.0)
+    error("gaussian_sampler: 'sigma_scale' must be positive.");
+
+  /* compute new image size and get memory for images */
+  if (in->xsize * scale > (double) UINT_MAX ||
+      in->ysize * scale > (double) UINT_MAX)
+    error("gaussian_sampler: the output image size exceeds the handled size.");
+  N = (unsigned int) ceil(in->xsize * scale);
+  M = (unsigned int) ceil(in->ysize * scale);
+  aux = new_image_double(N, in->ysize);
+  out = new_image_double(N, M);
+
+  /* sigma, kernel size and memory for the kernel */
+  sigma = scale < 1.0 ? sigma_scale / scale : sigma_scale;
+  /*
+     The size of the kernel is selected to guarantee that the
+     the first discarded term is at least 10^prec times smaller
+     than the central value. For that, h should be larger than x, with
+       e^(-x^2/2sigma^2) = 1/10^prec.
+     Then,
+       x = sigma * sqrt( 2 * prec * ln(10) ).
+   */
+  prec = 3.0;
+  h = (unsigned int) ceil(sigma * sqrt(2.0 * prec * log(10.0)));
+  n = 1 + 2 * h; /* kernel size */
+  kernel = new_ntuple_list(n);
+
+  /* auxiliary double image size variables */
+  double_x_size = (int) (2 * in->xsize);
+  double_y_size = (int) (2 * in->ysize);
+
+  /* First subsampling: x axis */
+  for (x = 0; x < aux->xsize; x++) {
+    /*
+       x   is the coordinate in the new image.
+       xx  is the corresponding x-value in the original size image.
+       xc  is the integer value, the pixel coordinate of xx.
+     */
+    xx = (double) x / scale;
+    /* coordinate (0.0,0.0) is in the center of pixel (0,0),
+       so the pixel with xc=0 get the values of xx from -0.5 to 0.5 */
+    xc = (int) floor(xx + 0.5);
+    gaussian_kernel(kernel, sigma, (double) h + xx - (double) xc);
+    /* the kernel must be computed for each x because the fine
+       offset xx-xc is different in each case */
+
+    for (y = 0; y < aux->ysize; y++) {
+      sum = 0.0;
+      for (i = 0; i < kernel->dim; i++) {
+        j = xc - h + i;
+
+        /* symmetry boundary condition */
+        while (j < 0) j += double_x_size;
+        while (j >= double_x_size) j -= double_x_size;
+        if (j >= (int) in->xsize) j = double_x_size - 1 - j;
+
+        sum += in->data[j + y * in->xsize] * kernel->values[i];
+      }
+      aux->data[x + y * aux->xsize] = sum;
+    }
+  }
+
+  /* Second subsampling: y axis */
+  for (y = 0; y < out->ysize; y++) {
+    /*
+       y   is the coordinate in the new image.
+       yy  is the corresponding x-value in the original size image.
+       yc  is the integer value, the pixel coordinate of xx.
+     */
+    yy = (double) y / scale;
+    /* coordinate (0.0,0.0) is in the center of pixel (0,0),
+       so the pixel with yc=0 get the values of yy from -0.5 to 0.5 */
+    yc = (int) floor(yy + 0.5);
+    gaussian_kernel(kernel, sigma, (double) h + yy - (double) yc);
+    /* the kernel must be computed for each y because the fine
+       offset yy-yc is different in each case */
+
+    for (x = 0; x < out->xsize; x++) {
+      sum = 0.0;
+      for (i = 0; i < kernel->dim; i++) {
+        j = yc - h + i;
+
+        /* symmetry boundary condition */
+        while (j < 0) j += double_y_size;
+        while (j >= double_y_size) j -= double_y_size;
+        if (j >= (int) in->ysize) j = double_y_size - 1 - j;
+
+        sum += aux->data[x + j * aux->xsize] * kernel->values[i];
+      }
+      out->data[x + y * out->xsize] = sum;
+    }
+  }
+
+  /* free memory */
+  free_ntuple_list(kernel);
+  free_image_double(aux);
+
+  return out;
+}
