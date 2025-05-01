43888dbb65001b1088f243f316146752e80e752c
François Costa <frankost.costa@gmail.com>
2025-05-01 20:53:22 +0200

    Add external parameters to run multiple experiments
    (cherry picked from commit ccf89ed934e87785b4de7f7129568109ea9cb129)

---
+++ src/parameters.h
@@ -0,0 +1,11 @@
+# ifndef PARAMETERS_H
+# define PARAMETERS_H
+
+constexpr bool with_gaussian = false;
+
+constexpr unsigned int stride_ll_angle_x = 4;
+constexpr unsigned int stride_ll_angle_y = 4;
+
+constexpr unsigned int numberThreads = 16;
+
+# endif
